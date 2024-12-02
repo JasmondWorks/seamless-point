ec85c560a0d3b9f2ffb4ed264bb7874465b4a7de
Obafemi Olorede <obafemilared@gmail.com>
2024-12-02 01:26:13 +0100

    added favicon

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -18,6 +18,40 @@ export default function RootLayout({
       className="scroll-smooth text-[85%] md:text[90%] lg:text-[95%]"
       style={{ scrollBehavior: "smooth" }}
     >
+      <head>
+        {/* Favicon setup */}
+        <link rel="icon" href="/assets/images/favicon/" />
+        <link
+          rel="icon"
+          href="/assets/images/favicon/favicon.ico"
+          sizes="any"
+        />
+        <link
+          rel="icon"
+          href="/assets/images/favicon/favicon-32x32.png"
+          sizes="32x32"
+        />
+        <link
+          rel="icon"
+          href="/assets/images/favicon/favicon-16x16.png"
+          sizes="16x16"
+        />
+        <link
+          rel="apple-touch-icon"
+          href="/assets/images/favicon/apple-touch-icon.png"
+          sizes="180x180"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          href="/assets/images/favicon/android-chrome-192x192.png"
+          sizes="192x192"
+        />
+        <link rel="manifest" href="/assets/images/favicon/site.webmanifest" />
+
+        {/* Other meta tags for iOS, Android, etc. */}
+        <meta name="theme-color" content="#ffffff" />
+      </head>
       <body className={`antialiased overflow-x-hidden no-scrollbar`}>
         <GoogleOAuthProvider
           clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID ?? ""}
